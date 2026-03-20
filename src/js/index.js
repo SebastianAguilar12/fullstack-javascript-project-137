3b7798a52ae3e58441ea19ea78aa2467358808af
SebastianAguilar12 <sebas09981@gmail.com>
2026-03-19 22:30:39 -0500

    remove webpack

--- src/js/index.js
+++
@@ -1,8 +0,0 @@
-const rssForm = document.querySelector('#rss-form');
-
-rssForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const userUrl = e.target.querySelector('#link').value;
-  fetch(userUrl)
-    .then((data) => console.log(data));
-});
